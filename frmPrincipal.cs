2237ca13dfc2e2026ec76013524922486ee89f9b
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-04-01 11:10:46 -0300

    dev: version 29

--- frmPrincipal.cs
+++ frmPrincipal.cs
@@ -15,7 +15,7 @@ namespace ProjectEX
         public frmPrincipal()
         {
             InitializeComponent();
-            lblVersion.Text = "1.0.28";
+            lblVersion.Text = "1.0.29";
         }
 
         private void leituraDeparaToolStripMenuItem_Click(object sender, EventArgs e)
