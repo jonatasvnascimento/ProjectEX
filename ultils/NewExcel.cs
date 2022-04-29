67066c013e069bd0de9f3ac1f03b718cefaf0d86
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-04-29 08:01:29 -0300

    dev: usando task para melhorar o processo

--- ultils/NewExcel.cs
+++ ultils/NewExcel.cs
@@ -22,42 +22,43 @@ namespace ProjectEX.ultils
             using (ExcelPackage excelPackage = new ExcelPackage(new FileInfo(path)))
             {
                 var sheet = excelPackage.Workbook.Worksheets[0];
-                var row = 1;
                 bool continuar = true;
-                while (continuar)
+                var tasks = new List<Task>();
+                for (var row = 1; row <= sheet.Dimension.Rows; row++)
                 {
-                    if (!String.IsNullOrEmpty(sheet.Cells[row, 2].Text))
+                    var task = Task.Run(async () =>
                     {
-                        CamposExcelModel campos = new CamposExcelModel();
+                        if (!string.IsNullOrEmpty(sheet.Cells[row + 1, 2].Text))
+                        {
+                            CamposExcelModel campos = new CamposExcelModel();
 
-                        campos.Barra =              valueField(sheet.Cells[row + 1, 2].Text).validaCampo(10);
-                        campos.NovoProduto =        valueField(sheet.Cells[row + 1, 3].Text).validaCampo(15);
-                        campos.ItemCTR =            valueField(sheet.Cells[row + 1, 4].Text).validaCampo(2);
-                        campos.Enxoval =            valueField(sheet.Cells[row + 1, 5].Text).validaCampo(15);
-                        campos.Descricao =          valueField(sheet.Cells[row + 1, 6].Text).validaCampo(60);
-                        campos.Cor =                valueField(sheet.Cells[row + 1, 7].Text).validaCampo(4);
-                        campos.Tamanho =            valueField(sheet.Cells[row + 1, 8].Text).validaCampo(2);
-                        campos.QtdHigienizações =   valueField(sheet.Cells[row + 1, 9].Text).validaCampo(8);
-                        campos.Cadastro =           valueField(sheet.Cells[row + 1, 10].Text).validaCampo(10, true);
-                        campos.Funcionario =        valueField(sheet.Cells[row + 1, 11].Text).validaCampo(6);
-                        campos.Nome =               valueField(sheet.Cells[row + 1, 12].Text).validaCampo(100);
-                        campos.NumArm =             valueField(sheet.Cells[row + 1, 13].Text).validaCampo(6, false, true);
-                        campos.NumGav =             valueField(sheet.Cells[row + 1, 14].Text).validaCampo(6);
-                        campos.Localização =        valueField(sheet.Cells[row + 1, 15].Text).validaCampo(20);
-                        campos.CodSet =             valueField(sheet.Cells[row + 1, 16].Text).validaCampo(6);
-                        campos.DescSetor =          valueField(sheet.Cells[row + 1, 17].Text).validaCampo(20);
-                        campos.Filial =             valueField(sheet.Cells[row + 1, 18].Text).validaCampo(2);
-                        campos.NovoContrato =       valueField(sheet.Cells[row + 1, 19].Text).validaCampo(6);
-                        campos.Contrato =           valueField(sheet.Cells[row + 1, 20].Text).validaCampo(50);
+                            campos.Barra = valueField(sheet.Cells[row + 1, 2].Text).validaCampo(10);
+                            campos.NovoProduto = valueField(sheet.Cells[row + 1, 3].Text).validaCampo(15);
+                            campos.ItemCTR = valueField(sheet.Cells[row + 1, 4].Text).validaCampo(2);
+                            campos.Enxoval = valueField(sheet.Cells[row + 1, 5].Text).validaCampo(15);
+                            campos.Descricao = valueField(sheet.Cells[row + 1, 6].Text).validaCampo(60);
+                            campos.Cor = valueField(sheet.Cells[row + 1, 7].Text).validaCampo(4);
+                            campos.Tamanho = valueField(sheet.Cells[row + 1, 8].Text).validaCampo(2);
+                            campos.QtdHigienizações = valueField(sheet.Cells[row + 1, 9].Text).validaCampo(8);
+                            campos.Cadastro = valueField(sheet.Cells[row + 1, 10].Text).validaCampo(10, true);
+                            campos.Funcionario = valueField(sheet.Cells[row + 1, 11].Text).validaCampo(6);
+                            campos.Nome = valueField(sheet.Cells[row + 1, 12].Text).validaCampo(100);
+                            campos.NumArm = valueField(sheet.Cells[row + 1, 13].Text).validaCampo(6, false, true);
+                            campos.NumGav = valueField(sheet.Cells[row + 1, 14].Text).validaCampo(6);
+                            campos.Localização = valueField(sheet.Cells[row + 1, 15].Text).validaCampo(20);
+                            campos.CodSet = valueField(sheet.Cells[row + 1, 16].Text).validaCampo(6);
+                            campos.DescSetor = valueField(sheet.Cells[row + 1, 17].Text).validaCampo(20);
+                            campos.Filial = valueField(sheet.Cells[row + 1, 18].Text).validaCampo(2);
+                            campos.NovoContrato = valueField(sheet.Cells[row + 1, 19].Text).validaCampo(6);
+                            campos.Contrato = valueField(sheet.Cells[row + 1, 20].Text).validaCampo(50);
 
-                        objretorno.Add(campos);
-                        row++;
-                    }
-                    else
-                    {
-                        continuar = false;
-                    }
+                            objretorno.Add(campos);
+                        }
+                    });
+
+                    tasks.Add(task);
                 }
+                Task.WaitAll(tasks.ToArray());
             }
             return objretorno;
         }
